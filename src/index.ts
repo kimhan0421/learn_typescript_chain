7fbb6f3e01550be47a217bdb14315cc4be07e3f3
hansol <kimhan0421@naver.com>
2020-12-25 01:12:19 +0900

    7. Creating a Block part Three

--- src/index.ts
+++ src/index.ts
@@ -13,7 +13,7 @@ class Block {
     timestamp: number,
     data: string
   ): string =>
-    CryptoJS.SHA256(index + previousHash + timestamp + data).toStrings();
+    CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
 
   constructor(
     index: number,
@@ -39,4 +39,28 @@ const getBlockchain = (): Block[] => blockchain;
 const getLatesBlock = (): Block => blockchain[blockchain.length - 1];
 
 const getNewTimeStamp = (): number => Math.round(new Date().getTime() / 1000);
+
+const createNewBlock = (data: string): Block => {
+  const previousBlock: Block = getLatesBlock();
+  const newIndex: number = previousBlock.index + 1;
+  const newTimestamp: number = getNewTimeStamp();
+  const newHash: string = Block.calculateBlockHash(
+    newIndex,
+    previousBlock.hash,
+    newTimestamp,
+    data
+  );
+  const newBlock: Block = new Block(
+    newIndex,
+    newHash,
+    previousBlock.hash,
+    data,
+    newTimestamp
+  );
+  return newBlock;
+};
+
+console.log(createNewBlock("hello"));
+console.log(createNewBlock("bye bye"));
+
 export {};
