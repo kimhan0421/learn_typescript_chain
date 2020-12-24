c7f0e844e5ad3e6c9d87551e500f6941dec5ebbc
hansol <kimhan0421@naver.com>
2020-12-24 18:52:54 +0900

    4. Classes on Typescript part One

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,20 @@
-interface Human {
-  name: string;
-  age: number;
-  gender: string;
+class Human {
+  public name: string;
+  public age: number;
+  public gender: string;
+  constructor(name: string, age: number, gender?: string) {
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+  }
 }
 
-const person = {
-  name: "Lily",
-  age: 22,
-  gender: "female",
-};
+const lily = new Human("Lily", 22, "female");
 
 const sayHi = (person: Human): string => {
   return `Hello, ${person.name}. You are ${person.age}, ${person.gender}`;
 };
 
-console.log(sayHi(person));
+console.log(sayHi(lily));
 
 export {};
